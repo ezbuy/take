ec91070b619487aafdcf941697837ca2a20195d4
翁伟 <wengwei@ezbuy.com>
2017-11-11 19:55:46 +0800

    Add Unique methods

--- slice.go
+++ slice.go
@@ -2,6 +2,94 @@ package take
 
 import "reflect"
 
+func StringUnique(items interface{}, fn func(i int) string) []string {
+	rv := reflect.ValueOf(items)
+	size := rv.Len()
+
+	if size < 512 {
+		return stringUniqueFor(size, fn)
+	}
+
+	return stringUniqueMap(size, fn)
+}
+
+func stringUniqueMap(size int, fn func(i int) string) []string {
+	result := make([]string, 0, size)
+	checkMap := make(map[string]bool)
+
+	for i := 0; i < size; i++ {
+		item := fn(i)
+		if _, ok := checkMap[item]; !ok {
+			result = append(result, item)
+			checkMap[item] = true
+		}
+	}
+	return result
+}
+
+func stringUniqueFor(size int, fn func(i int) string) []string {
+	result := make([]string, 0, size)
+
+	for i := 0; i < size; i++ {
+		item := fn(i)
+		found := false
+		for _, existingItem := range result {
+			if item == existingItem {
+				found = true
+				break
+			}
+		}
+		if !found {
+			result = append(result, item)
+		}
+	}
+	return result
+}
+
+func Int64Unique(items interface{}, fn func(i int) int64) []int64 {
+	rv := reflect.ValueOf(items)
+	size := rv.Len()
+
+	if size < 512 {
+		return int64UniqueFor(size, fn)
+	}
+
+	return int64UniqueMap(size, fn)
+}
+
+func int64UniqueMap(size int, fn func(i int) int64) []int64 {
+	result := make([]int64, 0, size)
+	checkMap := make(map[int64]bool)
+
+	for i := 0; i < size; i++ {
+		item := fn(i)
+		if _, ok := checkMap[item]; !ok {
+			result = append(result, item)
+			checkMap[item] = true
+		}
+	}
+	return result
+}
+
+func int64UniqueFor(size int, fn func(i int) int64) []int64 {
+	result := make([]int64, 0, size)
+
+	for i := 0; i < size; i++ {
+		item := fn(i)
+		found := false
+		for _, existingItem := range result {
+			if item == existingItem {
+				found = true
+				break
+			}
+		}
+		if !found {
+			result = append(result, item)
+		}
+	}
+	return result
+}
+
 func String(items interface{}, fn func(i int) string) []string {
 	rv := reflect.ValueOf(items)
 	size := rv.Len()
--- slice_test.go
+++ slice_test.go
@@ -25,6 +25,31 @@ func TestString(t *testing.T) {
 	}
 }
 
+func TestStringUnique(t *testing.T) {
+	books := []*Book{
+		&Book{
+			Name: "foo",
+		},
+		&Book{
+			Name: "Bar",
+		},
+		&Book{
+			Name: "foo",
+		},
+		&Book{
+			Name: "Bar",
+		},
+	}
+
+	names := StringUnique(books, func(i int) string {
+		return books[i].Name
+	})
+
+	if names[0] != "foo" || names[1] != "Bar" {
+		t.Error("take string failed")
+	}
+}
+
 type (
 	Item struct {
 		ID int64
@@ -67,6 +92,28 @@ func BenchmarkTake(b *testing.B) {
 	b.ResetTimer()
 	for c := 0; c < b.N; c++ {
 		ids := Int64(items, func(i int) int64 {
+			return items[i].ID
+		})
+		_ = ids
+	}
+}
+
+func BenchmarkTakeUniqueFor(b *testing.B) {
+	items := testItems()
+	b.ResetTimer()
+	for c := 0; c < b.N; c++ {
+		ids := int64UniqueFor(len(items), func(i int) int64 {
+			return items[i].ID
+		})
+		_ = ids
+	}
+}
+
+func BenchmarkTakeUniqueMap(b *testing.B) {
+	items := testItems()
+	b.ResetTimer()
+	for c := 0; c < b.N; c++ {
+		ids := int64UniqueMap(len(items), func(i int) int64 {
 			return items[i].ID
 		})
 		_ = ids
