d9f00c85bc5923df33032c99fe00bd5c284e3c3d
翁伟 <wengwei@ezbuy.com>
2017-11-11 19:35:52 +0800

    Add benchmark

--- slice_test.go
+++ slice_test.go
@@ -24,3 +24,51 @@ func TestString(t *testing.T) {
 		t.Error("take string failed")
 	}
 }
+
+type (
+	Item struct {
+		ID int64
+		S  string
+	}
+)
+
+func testItems() []Item {
+	items := make([]Item, 1000)
+	for i := range items {
+		items[i] = Item{ID: int64(i), S: "test string"}
+	}
+	return items
+}
+
+func BenchmarkIndex(b *testing.B) {
+	items := testItems()
+	b.ResetTimer()
+	for c := 0; c < b.N; c++ {
+		ids := make([]int64, len(items))
+		for i := range ids {
+			ids[i] = items[i].ID
+		}
+	}
+}
+
+func BenchmarkAppend(b *testing.B) {
+	items := testItems()
+	b.ResetTimer()
+	for c := 0; c < b.N; c++ {
+		ids := make([]int64, 0, len(items))
+		for i := range items {
+			ids = append(ids, items[i].ID)
+		}
+	}
+}
+
+func BenchmarkTake(b *testing.B) {
+	items := testItems()
+	b.ResetTimer()
+	for c := 0; c < b.N; c++ {
+		ids := Int64(items, func(i int) int64 {
+			return items[i].ID
+		})
+		_ = ids
+	}
+}
